b60d7a554aa149b5addb2b6cca4c2d48a80654a2
Wimukthi316 <wimukthi316@gmail.com>
2025-03-12 23:36:52 +0530

    About Us Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/user/Home';
+import AboutUs from './pages/user/AboutUs';
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
   );
