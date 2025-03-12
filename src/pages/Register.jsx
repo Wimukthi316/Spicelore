ab377c3f2bd3c2152780834644b1fc6edf30194d
Wimukthi316 <wimukthi316@gmail.com>
2025-03-12 11:36:31 +0530

    re-Design the Registration form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,7 +62,7 @@ const Registration = () => {
         <>
             <Navbar />
 
-            <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/registerbackground.jpg')" }}>
+            <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/Registerbackground.jpg')" }}>
                 <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md" style={{ backgroundColor: "rgba(255, 255, 255, 0.7)" }}>
                     <h2 className="text-2xl font-bold text-[#351108] mb-6 text-center">Create Your Account</h2>
 
