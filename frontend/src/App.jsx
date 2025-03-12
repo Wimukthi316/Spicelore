b98ecc71b26454b62fe210a083730393dcb724bc
Wimukthi316 <wimukthi316@gmail.com>
2025-03-12 23:42:19 +0530

    All Done Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/user/Home';
 import AboutUs from './pages/user/AboutUs';
 import ContactUs from './pages/user/ContactUs';
+import Login from './pages/user/Login';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
